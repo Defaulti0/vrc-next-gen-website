30ca890c6072bee0a01925192edaa57a77b1535d
Defaulti0 <crismartin994@gmail.com>
2024-10-12 15:16:35 -0400

    Initial Commit

---
+++ src/pages/games.js
@@ -0,0 +1,10 @@
+import { Button } from "@material-tailwind/react";
+
+export default function Games() {
+  return (
+    <div>
+      <h1>Hello, Games Page!</h1>
+      <Button>Button</Button>
+    </div>
+  );
+}
